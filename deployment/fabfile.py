e3bc80bb44f7abfad7364796f92c98fc16ee2ff9
Warren Mar <warren.mar@gmail.com>
2014-09-09 20:26:43 -0700

    fabfile for setting up shxreader

---
+++ deployment/fabfile.py
@@ -0,0 +1,40 @@
+"""
+Setup new ubuntu 14.04 server to run SHX Reader
+"""
+from fabric.api import run, local, sudo, env
+from fabric.decorators import task, parallel
+from fabric.colors import green, red, yellow
+from fabric.context_managers import cd
+from fabric.contrib.files import exists
+
+
+@task
+def setup():
+    """Install dependencies"""
+    sudo('apt-get update')
+    sudo('apt-get install libtool autoconf automake uuid-dev build-essential lynx-cur -y')
+    sudo('apt-get install python-pip -y')
+    setup_nltk()
+    setup_db()
+    setup_crawl()
+
+
+@task
+def setup_nltk():
+    sudo('pip install nltk')
+
+
+@task
+def setup_db():
+    sudo('apt-get install mariadb-server -y')
+    sudo('apt-get install python-mysqldb -y')
+    sudo('apt-get install redis-server -y')
+
+
+@task
+def setup_crawl():
+    sudo('apt-get install python-lxml -y')
+    sudo('pip install requests')
+    sudo('pip install selenium')
+    sudo('pip install beautifulsoup4')
+    sudo('pip install rq')
