2f4aea08a97299bd5c0d674bea3f0813cf2773c9
Warren Mar <warren.mar@gmail.com>
2014-09-20 17:32:36 -0700

    add user agent string

---
+++ crawler/__init__.py
@@ -0,0 +1,3 @@
+CHROME_USER_AGENT = 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
+GOOGLEBOT_USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.122 Safari/537.36'
+FIREFOX_USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:32.0) Gecko/20100101 Firefox/32.0'
