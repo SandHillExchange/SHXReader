cef29832d4183a5901d8b16194f9b7721bd6e17d
Warren Mar <warren.mar@gmail.com>
2014-09-22 09:24:32 -0700

    fix schema

--- owler/schema.sql
+++ owler/schema.sql
@@ -6,8 +6,10 @@ CREATE TABLE owler (
 	organization VARCHAR(80),
 	owler_url VARCHAR(256),
 	url VARCHAR(256),
-    heading VARCHAR(256),
+    headline VARCHAR(256),
     source VARCHAR(50),
     duration VARCHAR(8),
     timestamp INT
 );
+
+
