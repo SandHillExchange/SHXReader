9946fedd71c12afa74c58a71845244d0b193b4df
Warren Mar <warren.mar@gmail.com>
2014-09-22 01:03:00 -0700

    fix uploading

--- shxreader.py
+++ shxreader.py
@@ -2,15 +2,16 @@
 """SHXReader
 """
 import owler.crawl as owler_crawler
+from owler.crawl import ORGANIZATION_TO_URL
 import shx.api as shx_api
 
 def run():
-    owler_crawler.run()
-    # results = owler_crawler.get_articles('warby-parker')
-    # for r in results:
-    #     print r
-    #     shx_api.upload_news(r)
-    #     break
+    # owler_crawler.run()
+    for organization in ORGANIZATION_TO_URL:
+        results = owler_crawler.get_articles(organization)
+        for r in results:
+            print r
+            shx_api.upload_news(r)
 
 
 def main():
