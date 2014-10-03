1606bb50097e619ca169a9fbac150fef93f4818f
Warren Mar <warren.mar@gmail.com>
2014-10-03 16:57:05 -0700

    crawler

---
+++ shxcrawler.py
@@ -0,0 +1,28 @@
+import rss.feeds as rss_feeds
+from redis import Redis
+from rq import Queue
+import crawler.spider as spyder
+from crawler.spider import crawl_and_store_page
+
+
+def queue_urls(urls):
+    q = Queue(connection=Redis())
+    for url in urls:
+        if spyder.lookup_page(url) is None:
+            result = q.enqueue(crawl_and_store_page, url)
+
+
+def main():
+    urls = rss_feeds.techcrunch()
+    print urls
+    queue_urls(urls)
+
+    urls = rss_feeds.venturebeat()
+
+    urls = rss_feeds.hacker_news()
+
+    urls = rss_feeds.mashable()
+
+
+if __name__ == '__main__':
+    main()
