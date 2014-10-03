ac20242383f6f13ec37f4df958606145745f876c
Warren Mar <warren.mar@gmail.com>
2014-10-03 16:40:36 -0700

    added venturebeat

--- rss/feeds.py
+++ rss/feeds.py
@@ -3,13 +3,21 @@ from bs4 import BeautifulSoup
 import requests
 
 
-def techcrunch():
-    url = 'http://techcrunch.com/feed/'
+def urls_from_xml_feed(url):
     xml = BeautifulSoup(requests.get(url).text, features="xml")
     urls = [article.find('link').text for article in xml.findAll('item')]
     urls = [url.replace('?ncid=rss', '') for url in urls]
     return urls
 
+
+def techcrunch():
+    url = 'http://techcrunch.com/feed/'
+    return urls_from_xml_feed(url)
+
+
+def venturebeat():
+    url = 'http://venturebeat.com/feed/'
+    return urls_from_xml_feed(url)
 
 def main():
     pass
