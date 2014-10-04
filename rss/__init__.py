892cf6526b6aaaa9abc605e5bacea7e5eebfa4e4
Warren Mar <warren.mar@gmail.com>
2014-10-04 00:19:11 -0700

    add more feeds

--- rss/__init__.py
+++ rss/__init__.py
@@ -11,5 +11,13 @@ STANDARD_FEEDS = {
     'mercurynews': 'http://feeds.mercurynews.com/mngi/rss/CustomRssServlet/568/200774.xml',
     'bloombergview': 'http://www.bloombergview.com/rss',
     'forbes': 'http://www.forbes.com/technology/index.xml',
+    'timesofindia': 'http://timesofindia.feedsportal.com/c/33039/f/533923/index.rss',
+    'coindesk': 'http://www.coindesk.com/feed/',
+    'theatlantic': 'http://feeds.feedburner.com/AtlanticScienceAndTechnology?format=xml',
+    'beatbeat': 'http://betabeat.com/feed/',
+    'fortune': 'http://fortune.com/feed/',
+    'recode': 'http://recode.net/feed/',
     # 'gigaom': 'https://gigaom.com/feed/', invalid SSL
     'wsj': 'http://online.wsj.com/xml/rss/3_7455.xml'}
+
+# http://www.theverge.com/rss/frontpage, different do later
