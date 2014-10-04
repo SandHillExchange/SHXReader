131d8e58de7b65259f928df4fe96cbeabbf56d98
Warren Mar <warren.mar@gmail.com>
2014-10-03 19:54:42 -0700

    add rate limit

--- crawler/spider.py
+++ crawler/spider.py
@@ -6,9 +6,15 @@ from urlparse import urlparse
 from datetime import datetime
 import MySQLdb as mdb
 from crawler import CHROME_USER_AGENT
+from bs4 import BeautifulSoup
+from crawler.spider.ratelimiter import rate_limited
 
 
 SPIDER_DATA = '/mnt/shxreader/spider/data'
+
+
+def page_id_to_path(page_id, domain, timestamp):
+    return '%s/%s/%s/%s' % (SPIDER_DATA, domain, timestamp.strftime('%Y-%m-%d'), page_id)
 
 
 def lookup_page(url):
@@ -21,6 +27,17 @@ def lookup_page(url):
             return cur.fetchone()
 
 
+def lookup_by_domain(domain):
+    con = mdb.connect('localhost', 'shxreader', 'shxreader', 'shxreader')
+    with con:
+        cur = con.cursor(mdb.cursors.DictCursor)
+        prepared_statement = "select * from crawler where domain = %s"
+        r = cur.execute(prepared_statement, (domain, ))
+        if r > 0:
+            return cur.fetchall()
+
+
+@rate_limited(1)
 def crawl_page(url, user_agent=CHROME_USER_AGENT):
     headers = {'User-Agent': user_agent}
     resp = requests.get(url, headers=headers)
@@ -40,11 +57,11 @@ def store_page(url, page_source, user_agent=CHROME_USER_AGENT):
         page_id = cur.lastrowid
 
     # write source to file
-    datestamp = datetime.utcnow().strftime('%Y-%m-%d')
-    directory = '%s/%s/%s/' % (SPIDER_DATA, domain, datestamp)
+    timestamp = datetime.utcnow()
+    directory = '%s/%s/%s/' % (SPIDER_DATA, domain, timestamp.strftime('%Y-%m-%d'))
     if not os.path.exists(directory):
         os.makedirs(directory)
-    filename = '%s/%s/%s/%s' % (SPIDER_DATA, domain, datestamp, page_id)
+    filename = page_id_to_path(page_id, domain, timestamp)
     with open(filename, 'w') as f:
         f.write(page_source.encode('utf-8'))
 
@@ -53,6 +70,40 @@ def crawl_and_store_page(url, user_agent=CHROME_USER_AGENT):
     if lookup_page(url) is not None:
         page_source = crawl_page(url, user_agent)
         store_page(url, page_source, user_agent)
+
+
+# Analysis
+
+
+def get_links(soup):
+    links = [a['href'] for a in soup.findAll('a', href=True)]
+    urls = [url for url in links if url.startswith('http')]
+    return urls
+
+
+def get_links_by_pageid(pageid):
+    soup = BeautifulSoup(load_page(pageid))
+    return get_links(soup)
+
+
+def load_page_metadata(page_id):
+    con = mdb.connect('localhost', 'shxreader', 'shxreader', 'shxreader')
+    with con:
+        cur = con.cursor(mdb.cursors.DictCursor)
+        prepared_statement = "select * from crawler where id = %s"
+        r = cur.execute(prepared_statement, (page_id, ))
+        if r > 0:
+            return cur.fetchone()
+
+
+def load_page(page_id):
+    d = load_page_metadata(page_id)
+    if d is not None:
+        domain = d['domain']
+        datestamp = datetime.fromtimestamp(d['crawl_time'])
+        filename = page_id_to_path(page_id, domain, datestamp)
+        with open(filename) as f:
+            return f.read()
 
 
 if __name__ == '__main__':
--- shxcrawler.py
+++ shxcrawler.py
@@ -13,10 +13,20 @@ def queue_urls(urls):
             result = q.enqueue(crawl_and_store_page, url)
 
 
+def crawl_deeper(domain):
+    results = spyder.lookup_by_domain(domain)
+    for r in results:
+        urls = spyder.get_links_by_pageid(r['id'])
+        queue_urls(urls)
+
+
 def main():
+    print 'techcrunch'
     urls = rss_feeds.techcrunch()
     queue_urls(urls)
+    crawl_deeper('techcrunch.com')
 
+    print 'venturebeat'
     urls = rss_feeds.venturebeat()
     queue_urls(urls)
 
