c8f67ee8d521b14cc4d748eee9b3ddf4e8cbd8f8
Warren Mar <warren.mar@gmail.com>
2015-02-06 08:46:28 -0800

    fix url getting

--- owler/crawl.py
+++ owler/crawl.py
@@ -68,10 +68,11 @@ def get_url_from_owler_article_page(url):
     print url
     try:
         soup = BeautifulSoup(urllib.urlopen(url).read())
-        script = soup.findAll('script')[-1]
-        m = re.search('location = "(?P<url>.+)"', str(script))
-        if m is not None:
-            return m.group('url')
+        return soup.find('iframe')['src']
+        # script = soup.findAll('script')[-1]
+        # m = re.search('location = "(?P<url>.+)"', str(script))
+        # if m is not None:
+        #     return m.group('url')
     except IOError:
         print 'Error with ' + url
 
